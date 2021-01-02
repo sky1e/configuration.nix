1b7eb79af97e84f7bea1c24ed97dc8c5be5326ac
Skye Jensen <skyeajensen@gmail.com>
2021-01-02 16:45:22 -0500

    Updates

---
+++ users/skye.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+{
+  isNormalUser = true;
+  extraGroups = [ "wheel" "networkmanager" ];
+  shell = pkgs.fish;
+  uid = 1000;
+  hashedPassword = import ../password-hash.nix;
+  packages = let home = config.users.users.skye.home;
+  in import (home + "/.config/nixpkgs/packages.nix" ) {
+    pkgs = import <nixpkgs> {
+      config = import (home + "/.config/nixpkgs/config.nix");
+      overlays = import (home + "/.config/nixpkgs/overlays.nix");
+    };
+  };
+}
